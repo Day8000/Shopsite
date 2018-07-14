4b8aca82ef995ed581f2f871ca8fdc7aedd28262
David Senior <d-senior@hotmail.com>
2018-07-14 14:38:50 -0600

    added pages and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root	"pages#home"
+  get "eat" => "pages#eat"	
+  get	"services" => "pages#services"
+  get "hotels" => "pages#hotels"
+  get "retail" => "pages#retail"
+  get "vehicles" => "pages#vehicles"
+  get "contact" => "pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
